4ea65a06e21a5f13aa4cf571d558304bc227e19c
杨蓬勃 <yangpengbo2636@gmail.com>
2019-03-02 21:09:02 -0800

    智能指针

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
+using namespace std;
 
 int main()
 {
     printf("====Learn Boost !====\n");
+
+    std::vector<boost::shared_ptr<int> > v;
+    v.push_back(boost::shared_ptr<int>(new int(1)));
+    v.push_back(boost::shared_ptr<int>(new int(2)));
+
+    for (int i = 0;i < v.size();i++)
+    {
+          printf("v = %d \n", * v[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
